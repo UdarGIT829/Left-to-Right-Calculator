efdb6def9ce60c24433098f6a31a92034d0e17e2
Viraat Udar <viraatudar9@gmail.com>
2023-03-09 23:58:27 -0800

    Modified #ifndef statement to reflect file type, removed outdated version of product function, and matched formatting with DecimalOperators.

--- IntegerOperators.hpp
+++ IntegerOperators.hpp
@@ -1,6 +1,6 @@
 //Header for basic integer operations
-#ifndef INTEGEROPERATORS_H
-#define INTEGEROPERATORS_H
+#ifndef INTEGEROPERATORS_HPP
+#define INTEGEROPERATORS_HPP
 //Include Abstract Class for Calculator
 #include "AbstractionCalculator.hpp"
 
@@ -8,26 +8,18 @@ class IntegerCalculator: public Calculator{
 
     public:
         void sum(std::string arg1, std::string arg2){
-            
             int thisSum = std::stoi(arg1) + std::stoi(arg2);
             this->result = std::to_string(thisSum);
         }
 
         void difference(std::string arg1, std::string arg2){
-            
             int thisDifference = std::stoi(arg1) - std::stoi(arg2);
             this->result = std::to_string(thisDifference);
         }
 
         void product(std::string arg1, std::string arg2){
-            
             int thisProduct = std::stoi(arg1) * std::stoi(arg2);
             this->result = std::to_string(thisProduct);
         }
-
-        // int product(int arg1, int arg2){
-        //     int thisProduct = arg1 * arg2;
-        //     return thisProduct;
-        // }
 };
 #endif
