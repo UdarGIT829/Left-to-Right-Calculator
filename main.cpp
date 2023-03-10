d1e39f22014dc268165b23123f83eea096de2174
Viraat Udar <viraatudar9@gmail.com>
2023-03-10 00:07:22 -0800

    The dual calculator and abstraction class minimized code needed for testing :)

--- main.cpp
+++ main.cpp
@@ -4,63 +4,35 @@
 //string for converting function results for Terminal I/O
 #include <string>
 
-#include "IntegerOperators.hpp"
+#include "DualCalculator.hpp"
 
-#include "DecimalOperators.hpp"
-
-#include "Typechecking.hpp"
 
 
 int main() {
   // TESTING SUM FUNCTIONS
   //=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-    //Initialize Calculators
-  IntegerCalculator myIntCalc;
-  DecimalCalculator myDecCalc;
-  Calculator* myCalculator;
+    //Initialize Calculator
+  DualCalculator myDualCalc;
     //Declare integer and decimal inputs
   std::string myIntInput1 = "5";
   std::string myIntInput2 = "13";
 
-  std::string myDecInput1 = "5.00001";
+  std::string myDecInput1 = "5.22222";
   std::string myDecInput2 = "29.999998";
 
-  myCalculator = &myIntCalc;
-  myCalculator->sum(myIntInput1, myIntInput2);
-  std::string myIntResult = myCalculator->result;
-
-  myCalculator = &myDecCalc;
-  myCalculator->sum(myDecInput1, myDecInput2);
-  std::string myDecResult = myCalculator->result;
-
-  std::cout << "The int sum is: " << myIntResult << std::endl;
-  std::cout << "The double sum is: " << myDecResult << std::endl;
-
+  std::cout << myDualCalc.sum(myIntInput1, myIntInput2) << std::endl;
+  std::cout << myDualCalc.sum(myDecInput1, myDecInput2) << std::endl;
   std::cout << std::endl;
 
-  myCalculator = &myIntCalc;
-  myCalculator->difference(myIntInput1, myIntInput2);
-  myIntResult = myCalculator->result;
-
-  myCalculator = &myDecCalc;
-  myCalculator->difference(myDecInput1, myDecInput2);
-  myDecResult = myCalculator->result;
-
-  std::cout << "The int difference is: " << myIntResult << std::endl;
-  std::cout << "The double difference is: " << myDecResult << std::endl;
-
+  std::cout << myDualCalc.difference(myIntInput1, myIntInput2) << std::endl;
+  std::cout << myDualCalc.difference(myDecInput1, myDecInput2) << std::endl;
   std::cout << std::endl;
 
-  myCalculator = &myIntCalc;
-  myCalculator->product(myIntInput1, myIntInput2);
-  myIntResult = myCalculator->result;
+  std::cout << myDualCalc.product(myIntInput1, myIntInput2) << std::endl;
+  std::cout << myDualCalc.product(myDecInput1, myDecInput2) << std::endl;
+  std::cout << std::endl;
 
-  myCalculator = &myDecCalc;
-  myCalculator->product(myDecInput1, myDecInput2);
-  myDecResult = myCalculator->result;
 
-  std::cout << "The int product is: " << myIntResult << std::endl;
-  std::cout << "The double product is: " << myDecResult << std::endl;
   //=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
   return 0;
 }
